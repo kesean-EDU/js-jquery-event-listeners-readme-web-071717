39ba01b354b86ef52ca2a2101ae4635e19c1cb92
kewoodh <kesean.woodhouse@gmail.com>
2017-09-08 10:01:42 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', () =>{
+    alert('Hey!')
+  })
+}
 
-// call functions here
+function frameIt() {
+  $('img').on('load', (e)=>{
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', (key) =>{
+    if (key.which === 71) {
+      alert('G was pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', (e)=>{
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
+$(document).ready(function(){
 
 });
